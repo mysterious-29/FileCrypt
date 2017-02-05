5aeda392bf7287101a2148f1ebe5d613e9ca8e8a
Aditya <aadityaaaaroraa@gmail.com>
2017-02-05 11:28:43 +0530

    Navigation Menu and Overflow Menu Options added and few minor bugs fixed.

---
+++ app/src/main/java/com/example/mysterious/filecrypt/Activities/TeamActivity.java
@@ -0,0 +1,21 @@
+package com.example.mysterious.filecrypt.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.example.mysterious.filecrypt.R;
+
+import static com.example.mysterious.filecrypt.R.id.teamToolbar;
+
+public class TeamActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team);
+
+
+    }
+}
