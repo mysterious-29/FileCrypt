be5774d7362d809de361b40c11b242363e80df25
spider34 <rakekum34@gmail.com>
2017-02-05 15:16:59 +0530

    Image Add to Vault feature added

--- app/src/main/java/com/example/mysterious/filecrypt/Activities/AddToVaultActivity.java
+++ app/src/main/java/com/example/mysterious/filecrypt/Activities/AddToVaultActivity.java
@@ -1,112 +1,73 @@
 package com.example.mysterious.filecrypt.Activities;
 
-import android.content.Context;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v4.widget.CursorAdapter;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-import com.example.mysterious.filecrypt.Fragments.AddToDocVaultFragment;
-import com.example.mysterious.filecrypt.Fragments.AddToImgVaultFragment;
-import com.example.mysterious.filecrypt.Fragments.AddToVidVaultFragment;
 import com.example.mysterious.filecrypt.R;
 
 public class AddToVaultActivity extends AppCompatActivity {
 
-    private DrawerLayout mDrawerLayout;
-    private ActionBarDrawerToggle mToggle;
 
-    TabLayout tabLayout;
-    ViewPager viewPager;
+    ImageView imgVw;
+    Button btn;
+
+    private int PICK_IMAGE_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addtovault);
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
 
-        mDrawerLayout.addDrawerListener(mToggle);
-        mToggle.syncState();
+        btn = (Button) findViewById(R.id.buttonLoadPicture);
+        imgVw = (ImageView) findViewById(R.id.imgView);
 
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        viewPager = (ViewPager) findViewById(R.id.viewPager);
-        viewPager.setAdapter(new CustomAdapter(getSupportFragmentManager(),getApplicationContext()));
-
-        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
-        tabLayout.setupWithViewPager(viewPager);
-
-        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-            }
+        btn.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
+            public void onClick(View arg0) {
 
-            }
+                Intent intent = new Intent();
 
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
 
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
             }
         });
     }
 
-    private class CustomAdapter extends FragmentPagerAdapter {
-
-        private String fragments [] = {"fragment1", "fragment2", "fragment3" };
-
-        public CustomAdapter(FragmentManager supportFragmentManager, Context applicationContext ){
-            super(supportFragmentManager);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            switch (position){
-                case 0:
-                    return new AddToDocVaultFragment();
-                case 1:
-                    return new AddToImgVaultFragment();
-                case 2:
-                    return new AddToVidVaultFragment();
-                default:
-                    return null;
-            }
-        }
-
-        @Override
-        public int getCount() {
-            return fragments.length;
-        }
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return fragments[position];
-        }
-    }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (mToggle.onOptionsItemSelected(item)){
-            return true;
-        }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        return super.onOptionsItemSelected(item);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri uri = data.getData();
+            String[] projection = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+            cursor.moveToFirst();
+
+
+            int columnIndex = cursor.getColumnIndex(projection[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+
+            imgVw.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+        }
     }
+
 }
